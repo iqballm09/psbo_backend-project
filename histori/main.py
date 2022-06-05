ebd3622a4cab1c1a038638fc4ded35b0d57fe64a
muammarpramudya <muammarpramudya@apps.ipb.ac.id>
2022-06-06 03:50:56 +0700

    mengedit histori main.py

--- histori/main.py
+++ histori/main.py
@@ -3,13 +3,10 @@ class Histori():
 		self.id = id
 		self.jam = jam
 		self.tanggal = tanggal
+
 	def getId(self):
 		return self.id 
 	def getJam(self):
         return self.jam
-    def setJam(self, jam):
-        self.jam = jam
     def getTanggal(self):
         return self.tanggal
-    def setTanggal(self, tanggal):
-        self.tanggal = tanggal
