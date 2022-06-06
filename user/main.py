fab5b1142849b0ed4384404069d8709825d1fab9
Tri Aji Handayani <78842271+TriAjiH@users.noreply.github.com>
2022-06-06 17:41:41 +0700

    Update main.py

--- user/main.py
+++ user/main.py
@@ -1,2 +1,30 @@
-class User:
-    pass
+class user():
+	def __init__(self, id, email, nama, deskripsi_singkat, password, no_telp, alamat, gambar, cover)
+		self.id = id
+		self.email = email
+		self.nama = nama
+		self.deskripsi_singkat = deskripsi_singkat
+		self.password = password
+		self.no_telp = no_telp
+		self.alamat = alamat
+		self.gambar = gambar
+		self.cover = cover
+
+	def getId(self) :
+		return self.id
+	def getEmail(self) :
+		return self.email
+	def getNama(self) :
+		return self.nama
+	def getDeskripsi_singkat(self) :
+		return self.deskripsi_singkat
+	def getPassword(self) :
+		return self.password
+	def getNo_telp(self) :
+		return self.no_telp
+	def getAlamat(self) :
+		return self.alamat
+	def getGambar(self) :
+		return self.gambar
+	def getCover(self) :
+		return self.cover
